4c7fa819b185c845c99dac8f2117dc2d26395b95
Юлия Фёдорова <uliafedorova297@gmail.com>
2022-10-06 22:50:38 +0300

    add *

---
+++ src/test/java/ru/netology/DzApi/MobileBankTest.java
@@ -0,0 +1,28 @@
+package ru.netology.DzApi;
+
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
+
+class MobileBankTest {
+
+    @Test
+    void shouldReturnDemoAccounts() {
+
+
+            given()
+                    .baseUri("http://localhost:9999/api/v1")
+                    // Выполняемые действия
+                    .when()
+                    .get("/demo/accounts")
+                    // Проверки
+                    .then()
+                    .statusCode(200)
+                    .body(matchesJsonSchemaInClasspath("accounts.schema.json"))
+            ;
+        }
+
+        }
